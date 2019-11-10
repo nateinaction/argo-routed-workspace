901ffc2af93c2b39b90f96c211ab0c044635d9dc
Nate Gay <email@nategay.me>
2019-11-10 14:57:29 -0600

    Change terraform backend

--- src/backend.tf
+++ src/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "worldpeace-terraform-state"
-    key            = "nategay_workspace.tfstate"
+    key            = "nategay_workspace_argo.tfstate"
     region         = "us-east-1"
     encrypt        = "true"
     dynamodb_table = "worldpeace-terraform-lock"
